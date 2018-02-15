0665555e01a81020dccbbf60e332ecb65ae4ff2a
Skarzynp <35232423+Skarzynp@users.noreply.github.com>
2018-02-15 14:34:40 -0500

    Add files via upload

---
+++ include/file_utilities.h
@@ -0,0 +1,6 @@
+#ifndef FILE_UTILITIES_H
+#define FILE_UTILITIES_H
+int read_file( char* filename, char  **buffer);
+int write_file( char* filename, char  *buffer, int size);
+
+#endif 
---
+++ include/functions.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void resize(char** buffer,int row, int col, char*** Board);
+void grow(char*** Board,int row,int col, char*** newB);
+
+#endif
